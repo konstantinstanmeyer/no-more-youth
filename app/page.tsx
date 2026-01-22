5b33f4431b644bbdeecbfd13a800ae9876573a09
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-22 10:00:34 -0500

    finally functional test version

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
 
 export default function Home() {
@@ -31,14 +32,52 @@ export default function Home() {
   ];
 
   return (
-    <main className="relative min-h-screen overflow-hidden">
+    <main className="relative min-h-screen">
+      <div
+        className={` top-0 h-[87vh] w-screen transition-opacity duration-400 delay-200 ${
+          isLoaded ? 'opacity-100' : 'opacity-0'
+        }`}
+      >
+        <div className={`w-full relative h-36 flex flex-row items-center px-10! text-white${
+          isLoaded ? 'opacity-100' : 'opacity-0'
+        } `}>
+          <div className="flex items-center gap-4 mr-auto! text-white">
+            <Link className="chivo font-black " href="/">MUSIC</Link>
+            <span className="text-xs">♥</span>
+            <Link className="chivo font-black text-white " href="/">SHOWS</Link>
+            <span className="text-xs">♦</span>
+            <Link className="chivo font-black text-white " href="/">VIDEOS</Link>
+          </div>
+          <div className="absolute left-1/2 -translate-x-1/2">
+            <Image
+              src="/no-more-youth-logo.png"
+              className="h-[100px] w-[500px]"
+              width={500}
+              height={100}
+              alt="NO MORE YOUTH"
+            />
+          </div>
+          <div className="flex items-center gap-4 ml-auto! text-white">
+            <Link className="chivo font-black text-white " href="/">MERCH</Link>
+            <span className="text-xs">♠</span>
+            <Link className="chivo font-black text-white " href="/">BANDSINTOWN</Link>
+            <span className="text-xs">♣</span>
+            <Link className="chivo font-black text-white " href="/">CONTACT</Link>
+          </div>
+        </div>
+      </div>
+      <div className={`h-[60vh] relative object-cover transition-opacity delay-400 duration-300 ${
+          isLoaded ? 'opacity-100' : 'opacity-0'
+        }`}>
+        <Image  className="bg-transparent" src="/fiex.png" height={1440} width={2560} alt="pic" />
+      </div>
       <div className="video-container">
         <video autoPlay muted loop playsInline>
           <source src="/no-more-youth.mp4" type="video/mp4" />
         </video>
         <div className="video-overlay" />
       </div>
-      <section className="relative min-h-screen flex flex-col items-center justify-center px-6">
+      {/* <section className="relative min-h-screen flex flex-col items-center justify-center px-6">
         <div 
           className={`text-center transition-all duration-100 delay-500 ${
             isLoaded ? 'opacity-100' : 'opacity-0'
@@ -52,7 +91,7 @@ export default function Home() {
             alt={"NO MORE YOUTH"}
           />
         </div>
-      </section>
+      </section> */}
     </main>
   );
 }
