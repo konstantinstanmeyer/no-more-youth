a2b435fd179ee5ab49bb251fd30dd533c519f40d
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-01-27 11:31:20 -0500

    fixing html background color

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html className="bg-black" lang="en">
       <body
         className={`antialiased ${roboto.variable} ${chivo.variable} ${montserrat.variable}`}
       >
